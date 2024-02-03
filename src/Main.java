c536a5a6cea6a76d70a75e8d8c42ed88af699621
Owin Abeysinghe <owinabeysinghe@gmail.com>
2024-02-02 19:23:56 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double priceOfPurchase = 100.00;
+        double salesTax = 0;
+        salesTax = priceOfPurchase * (0.05);
+        System.out.println("The price of your item is $" + priceOfPurchase + " and the and the sales tax is $" + salesTax);
+    }
+}
